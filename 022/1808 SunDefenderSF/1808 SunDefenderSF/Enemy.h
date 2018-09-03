83a22705aa5446a6d7f8f765154f087a04b4d197
Carl Turner <42287840+roundbananas@users.noreply.github.com>
2018-09-03 15:24:37 +1000

    SunDefender: re-doing modes, slowly to find bug
    Created some issue with dangling pointer (maybe) when I created the modes.

---
+++ 022/1808 SunDefenderSF/1808 SunDefenderSF/Enemy.h
@@ -0,0 +1,92 @@
+//
+//  Enemy.h
+//  1808SunDefender
+//
+//  Created by Carl Turner on 27/8/18.
+//  Copyright © 2018 Carl Turner. All rights reserved.
+//
+
+#ifndef Enemy_h
+#define Enemy_h
+
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+class Enemy
+{
+private:
+    
+    Sprite m_EnemySprite;
+    Texture m_EnemyTexture;
+    
+    //How long does each enemy type stay in one spot, before moving?
+    const double NORMAL_LINGER = 2.5;  //seconds
+    const double STRONG_LINGER = 1.5;
+    const double SUPER_LINGER = 0.8;
+    
+    //Use colours to differentiate enemy types
+  //  const int NORMAL_COLOUR[3] = {255, 255, 0};   //yellow
+    const int NORMAL_COLOURR = 255; //yellow
+    const int NORMAL_COLOURG = 255;
+    const int NORMAL_COLOURB = 0;
+    
+    const int STRONG_COLOURR = 255;   //red
+    const int STRONG_COLOURG = 0;
+    const int STRONG_COLOURB = 0;
+    
+    const int SUPER_COLOURR = 255;    //purple
+    const int SUPER_COLOURG = 0;
+    const int SUPER_COLOURB = 255;
+    
+    //How much health does each enemy type start with?
+    const int NORMAL_HEALTH = 10;
+    const int STRONG_HEALTH = 20;
+    const int SUPER_HEALTH = 30;
+    
+    //Variables to describe an enemy's fixed characteristics
+    double m_LingerTime;   //how long they stay in one spot
+    int m_ColourR = 0;     //colour
+    int m_ColourG = 0;
+    int m_ColourB = 0;
+    int m_Colour[3] = {m_ColourR, m_ColourG, m_ColourB};
+    
+    //Variables to describe an enemy's dynamic characteristics
+    int m_Health;       //current health
+    int m_Position;     //current position (physical version)
+    Vector2f m_VPosition; //current position (digital version)
+
+    bool m_Alive;       //currently alive?
+    bool m_Shooting;    //currently shooting?
+    double m_ShootDuration = 2.0;    //they shoot for 2 seconds before moving on
+    
+    int m_type;         //for debugging
+    
+    double m_lastMoveUpdate; //in seconds
+    double m_lastShootUpdate; //in seconds
+
+    
+public:
+    //there's no default constructor?
+    Sprite getSprite();
+    
+    //Getter functions
+    bool isAlive();
+    bool isShooting();
+    double getShootDuration();
+
+    float getPositionX();
+    float getPositionY();
+    Vector2f getPosition();
+
+    int* getColour();    //returns an array
+    double getLinger();
+        
+    int getType();      //for debugging
+    
+    void spawnD(Vector2f pos, int enemyType, int seed); //for digital version
+    bool hit();         //handle enemy getting hit by projectile
+    void updateD(double t, VertexArray& mountainsVA); //for digital version
+};
+
+#endif /* Enemy_h */
---
+++ 022/1808 SunDefenderSF/1808 SunDefenderSF/SunSegment.h
@@ -0,0 +1,75 @@
+//
+//  SunSegment.h
+//  1808SunDefender
+//
+//  Created by Carl Turner on 27/8/18.
+//  Copyright © 2018 Carl Turner. All rights reserved.
+//
+//The sun has a total life: double m_SunLife. //in 'units' (use double because the Timer object uses a double.
+//Enemy lasers have an effect of draining sunlife at a rate: double m_EnemyLaserEffect; //in 'units per second'
+//So, if enemie lasers have shot the sun for a total of: m_SunLife/m_EnemyLaserEffect seconds, the sun will be completely drained.
+//So we need to create a variable to hold how many seconds the sun has been shot so far: double m_TotalTimeSunHasBeenLasered
+//We'll give visual feedback about the sun's status using a series of sprites. Each sprite will have varying degrees of sun coloured (alive), from 100% to 0.
+
+#ifndef SunSegment_h
+#define SunSegment_h
+
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+class SunSegment
+{
+private:
+    Sprite m_SunSprite;
+    Texture m_SunTexture;
+    Vector2f m_Position;
+    
+    double m_SunLife = 10000.0;           //in 'units'
+    double m_EnemyLaserEffect = 0.1;  //in 'units per second'
+    double m_TotalTimeSunHasBeenLasered = 0.0;    //for how many seconds has the sun has been lasered so far?
+    
+    double m_SunLifeRemaining;
+    bool m_SegmentAlive;
+    
+public:
+    SunSegment();
+    void spawn(Vector2f position);
+    Sprite getSprite();
+    
+    bool isAlive();
+    double getTotalLife();
+    double getTimeLasered();
+    double getLaserEffect();
+    double getLifeRemaining();
+    void update(int timeToAdd); //timeToAdd in seconds to total time sun has been lasered
+};
+#endif /* SunSegment_h */
+
+/*
+ //local variables
+ int sunArray[] {1, 2, 3, 4, 5};
+ int numSegments = sizeof(sunArray);
+ 
+ //SunSegment.h
+ class SunSegment
+ {
+ private:
+ 
+ public:
+ void spawn(int position, float size);
+ };
+ 
+ //BattleGround.h (to forward declare createSun in Engine.cpp
+ SunSegment* createSun(int numSegments, int sunArray);
+ 
+ //CreateSun.cpp
+ SunSegment* createSun(int numSegments, int sunArray)
+ {
+ SunSegment* sun = new SunSegment[numSegments];
+ sun[i].spawn (pos, size);
+ }
+ 
+ 
+ }
+ */
