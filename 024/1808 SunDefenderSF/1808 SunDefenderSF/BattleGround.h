b2e18523917b45e273e4925129239d073719f650
Carl Turner <42287840+roundbananas@users.noreply.github.com>
2018-09-05 14:33:55 +1000

    SunDefenderGame: finished modes. Fixed some mess
    Finished adding game modes. Discovered a variable I'd never initialised, nor updated, that was causing random game behaviour. Tidied up a few things to remove interdependency between classes.

---
+++ 024/1808 SunDefenderSF/1808 SunDefenderSF/BattleGround.h
@@ -0,0 +1,24 @@
+//
+//  BattleGround.h
+//  1808SunDefender
+//
+//  Created by Carl Turner on 27/8/18.
+//  Copyright © 2018 Carl Turner. All rights reserved.
+//
+
+#ifndef BattleGround_h
+#define BattleGround_h
+#include "Enemy.h"
+#include "Canon.h"
+#include "EnemyLaser.h"
+
+
+
+//These are forward declarations
+Enemy* createPlatoon(int numEnemies, int numMountains, VertexArray& mountainsVA);
+
+Canon* createPlayer(int numCanons, int NumMountains, VertexArray& canonVA);
+
+EnemyLaser* createEnemyLasers(VertexArray& laserVA, VertexArray& mountainsVA);
+
+#endif /* BattleGround_h */
