a9a66786960a722c502e7b139498a6e61f5457c9
Carl Turner <42287840+roundbananas@users.noreply.github.com>
2018-10-09 23:29:34 +1000

    MeetMe Game: player colours and explosions working
    Now to flesh out other play mode, scoring, HUD etc

---
+++ 050/1809MeetMe/Bullet.cpp
@@ -0,0 +1,123 @@
+//
+//  Bullet.cpp
+//
+//  Created by Carl Turner on 17/9/18.
+//
+#include "Bullet.h"
+
+Bullet::Bullet()
+{
+  
+
+}
+
+void Bullet::spawn()
+{
+  m_HeadAPos = -1.0f;
+  m_HeadBPos = 25.0f;
+  m_TrailLength = 5.0f;
+  m_AIsInFlight = false;
+  m_BIsInFlight = false;
+  m_Speed = 0.0f;
+  Serial.print ("SPAWN BULLET");
+}
+
+void Bullet::shoot(float bulletSpeed, long colour, float speedFactor)
+{
+ // Serial.println ("* BULLET: SHOOTING *");
+  m_AIsInFlight = true;
+  m_Speed = bulletSpeed * speedFactor;
+  
+  m_Colour = colour;
+
+/*
+  double factor = 0.15; //0.15 seems to work well for the second light, if the lead one is 255 brightness.
+
+  //set colour values for the trail
+  for (int i = 1 ; i < m_TrailLength; i++)
+  {
+    m_ColourR[i] = factor * colourArray[0];
+    m_ColourG[i] = factor * colourArray[1];
+    m_ColourB[i] = factor * colourArray[2];
+    factor *= 0.9;
+  }
+  */
+}
+
+void Bullet::updateBullet(int num_Pixels, unsigned long dt)
+{
+  //equation of motion, assuming zero acceleration. (Would be cool to add acceleration as they come to crash)
+  // position = previous_position + speed*dt
+  if(m_BIsInFlight)
+  {
+     m_HeadBPos -= m_Speed;
+     
+     if(m_HeadBPos < 0 - m_TrailLength)
+      {
+        m_BIsInFlight = false;
+      }
+  }
+  if (m_AIsInFlight)
+  {
+    if(m_HeadAPos >= num_Pixels - 1)
+    {
+      m_BIsInFlight = true;
+      
+      if(m_HeadAPos > num_Pixels + m_TrailLength)
+      {
+        m_AIsInFlight = false;
+      }
+    }
+  }
+ 
+  m_HeadAPos += m_Speed;
+}
+
+float Bullet::getHeadAPos()
+{
+     return m_HeadAPos; 
+}
+float Bullet::getTailAPos()
+{
+    return m_HeadAPos - m_TrailLength;
+}
+
+float Bullet::getHeadBPos()
+{
+     return m_HeadBPos; 
+}
+float Bullet::getTailBPos()
+{
+    return m_HeadBPos + m_TrailLength;
+}
+
+float Bullet::getTrailLength()
+{
+  return m_TrailLength;
+}
+
+bool Bullet::AIsInFlight()
+{
+  return m_AIsInFlight;
+}
+bool Bullet::BIsInFlight()
+{
+  return m_BIsInFlight;
+}
+void Bullet::StopA()
+{
+  m_AIsInFlight = false;
+}
+void Bullet::StopB()
+{
+  m_BIsInFlight = false;
+}
+float Bullet::getBulletSpeed()
+{
+    return m_Speed;
+}
+long Bullet::getColour(int index)
+{
+  //do something with index
+  return m_Colour;
+}
---
+++ 050/1809MeetMe/Render.cpp
@@ -0,0 +1,105 @@
+//
+//  Render.cpp
+//
+//  Created by Carl Turner on 20/9/18.
+//
+//APA102 led strip - 3.3V!!! If you supply 5V you'll get residual colour, can't turn LEDs off!
+
+#include "Engine.h"
+
+void Engine::m_Render()
+{  
+
+  if((m_mode == Modes::IDLE_MODE))
+  {  
+    Serial.println ("Render - IDLE_MODE");
+      
+      for (int i = 0; i < m_NumPlayers; i++) 
+      {
+        for (int j = 0; j < m_stripLengthArray[i]; j++) 
+        {
+          m_PlayerLEDS[i].setPixelColor(j, 0);
+        }
+      m_PlayerLEDS[i].show();
+      }
+  }
+
+  if((m_mode == Modes::COUNTDOWN))
+  {
+    Serial.println ("Render - COUNTDOWN");
+ 
+   
+  }
+  
+  if(m_mode == Modes::PLAYING)
+  {
+    for (int i = 0; i < m_NumPlayers; i++)
+    {
+      if (m_Players[i].isExploding()) //clear the strip
+      {  
+        for (int j = 0; j < m_stripLengthArray[i]; j++) m_PlayerLEDS[i].setPixelColor(j, 0);
+      }
+      else if (m_Players[i].getHasFired()) 
+      {
+         if (m_Bullets[i].AIsInFlight())
+         {
+              m_PlayerLEDS[i].setPixelColor(m_Bullets[i].getHeadAPos(), m_Bullets[i].getColour(0)); 
+              m_PlayerLEDS[i].setPixelColor(m_Bullets[i].getTailAPos(), 0);
+         } //end if partA 
+          
+         if (m_Bullets[i].BIsInFlight())
+         {
+              for (int k=0; k < m_NumPlayers; k++) //here, numPlayers refers to each LED strip
+              {
+                if (i == k)
+                {
+                  //that's the player whose bulletB is in flight, and we don't want to draw bullet B on their own strip
+                }
+                 else if (!m_Players[k].isExploding())//draw bullet B on every other strip , only if they're not exploding
+                 {
+                    m_PlayerLEDS[k].setPixelColor(m_Bullets[i].getHeadBPos(), m_Bullets[i].getColour(0)); 
+                    m_PlayerLEDS[k].setPixelColor(m_Bullets[i].getTailBPos(), 0);  
+                 }   
+              }//end for
+         } //end if for part B  
+    } //end if, to identify which player is associated with the bullet     
+  } //end loop through players
+
+   
+  //draw explosions
+  for (int i = 0; i < m_NumExplosions; i++)
+   {
+    if (m_Explosions[i].IsActive())
+    {
+     // Serial.print ("r "); Serial.print (r); Serial.print (" g "); Serial.print (g); Serial.print (" b "); Serial.print (b);
+      int strip = m_Explosions[i].GetStrip();
+      long colour = m_Explosions[i].GetColour();
+      for (int j = m_Explosions[i].GetTail(); j < m_Explosions[i].GetHead(); j++)
+      {
+         m_PlayerLEDS[strip].setPixelColor(j, colour); 
+      }
+  
+    }// end if explosion is active
+   }//end loop through explosions
+
+   for (int i = 0; i < m_NumPlayers; i++) m_PlayerLEDS[i].show();
+             
+    //UPDATE HUD
+       //    m_FramesSinceLastHUDUpdate++;
+    //  if(m_FramesSinceLastHUDUpdate > m_TargetFramesPerHUDUpdate)
+    //  {
+       //  m_FramesSinceLastHUDUpdate = 0;
+             
+  } //end PLAYING mode
+  
+}
+
+
+
+
+/*
+ * Syntax
+ * strip.setPixelColor(index, color);
+ *   strip.show();                     // Refresh strip
+
+ */
