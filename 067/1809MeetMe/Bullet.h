14258f03ba479b11b71805b5d37125a0cc5715d5
Carl Turner <42287840+roundbananas@users.noreply.github.com>
2018-11-10 21:30:32 +1000

    #MeetMe_Game: tried to remove 7SegDisplay flicker
    Tried to use a timer - eg (lastUpdate - millis() > period) - to decrease LED refresh rate (cos it takes so long and causes flicker on 7segDisplay). Couldn't find a sweet spot - the LEDs became jolty before the 7Seg got good enough.

---
+++ 067/1809MeetMe/Bullet.h
@@ -0,0 +1,61 @@
+//
+//  Bullet.h
+//
+//  Created by Carl Turner on 17/9/18.
+//
+//Bullet class manages movement and intensity of LEDs. Player holds the colour.
+
+#ifndef Bullet_h
+#define Bullet_h
+
+#include "Arduino.h"  
+
+class Bullet
+{
+  private:
+  
+    //index of first 'on' pixel and length of trail
+    float m_HeadAPos;     //position of head on player's own led strip
+    float m_HeadBPos;     //position of head on other player's led strips
+    float m_TrailLength;
+
+    bool m_AIsInFlight;
+    bool m_BIsInFlight;
+
+    //how fast does the bullet travel?
+    //this is the millisecond interval the bullet stays in one spot while moving
+    //in practice it's the time between calling the  updateBullet() function 
+    float m_Speed;
+
+    float m_BulletSpeedFactor = 0.02f; //fudge factor to allow enough sensivity on pot interface, while making the bullet fly at a reasonable speed.
+                              //was 0.04f. Provide a turbo option to boost it by double the speed instead.
+  
+    //create arrays to hold RGB values for each pixel between head and tail
+    // for sending to  Adafruit_DotStar::setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b)
+    long m_Colour;
+
+  public:
+    Bullet();
+    void spawn();
+    void shoot(float bulletSpeed, long colour);
+    void updateBullet(int num_Pixels, float bulletSpeed, unsigned long dt);  //updates both A and B bullets
+   
+    float getBulletSpeed(); 
+    
+    float getHeadAPos();
+    float getHeadBPos();
+    float getTailAPos();
+    float getTailBPos();
+    float getTrailLength();
+    
+    bool AIsInFlight();
+    bool BIsInFlight();
+
+    void StopA();
+    void StopB();
+
+    long getColour(int index); //returns colour of one piece of the bullet (ie one pixel)
+
+};
+
+#endif
---
+++ 067/1809MeetMe/FlasherStandAlone.cpp
@@ -0,0 +1,71 @@
+/*
+ * FlasherStandAlone.cpp
+ * 
+ * Created by Carl Turner on 8/11/18.
+ * 
+ * A single coloured LED that flashes. Not intended for use with RGB LEDs or LED strips
+ */
+
+#include "Arduino.h"
+#include "FlasherStandAlone.h"
+
+FlasherStandAlone::FlasherStandAlone()
+{
+}
+
+void FlasherStandAlone::Spawn(int pinNumber, long onDuration)
+{
+  m_pin = pinNumber;
+  pinMode(m_pin, OUTPUT);
+  m_OnDuration = onDuration;
+  m_OffDuration = onDuration;
+  m_LedMode = 0; //off
+  m_LedState = LOW;
+  m_PreviousMillis = 0;
+}
+
+int FlasherStandAlone::GetMode()
+{
+  return m_LedMode;
+}
+
+void FlasherStandAlone::SetMode(int mode)
+{
+  m_LedMode = mode;
+}
+
+void FlasherStandAlone::TurnOff()
+{ 
+  m_LedMode = 0; //constant off
+  m_LedState = LOW;
+  digitalWrite(m_pin, m_LedState);
+}
+
+void FlasherStandAlone::TurnOn()
+{
+  m_LedMode = 1; //constant on
+  m_LedState = HIGH;
+  digitalWrite(m_pin, m_LedState);
+}
+  
+void FlasherStandAlone::FlashLed()
+{
+  m_LedMode = 3; //flashing
+  unsigned long currentMillis = millis();
+
+  if((m_LedState == HIGH) && (currentMillis - m_PreviousMillis >= m_OnDuration))
+  {
+    m_LedState = LOW;
+    m_PreviousMillis = currentMillis;
+    digitalWrite(m_pin, m_LedState);
+  }
+  else if ((m_LedState == LOW) && (currentMillis - m_PreviousMillis >= m_OffDuration))
+  {
+    m_LedState = HIGH;
+    m_PreviousMillis = currentMillis;
+    digitalWrite(m_pin, m_LedState);
+  }
+}
+ 
+   //void FlashLed(long onDuration, long offDuration){} //if we want to have more control at some point
+  
