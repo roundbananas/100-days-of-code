3a3ce8b6ece2404cda60831d9c1e227efd8a498a
Carl Turner <42287840+roundbananas@users.noreply.github.com>
2019-02-05 11:56:15 +1000

    #Meet Me Game: final version. Done.

---
+++ 084/190205 Fully Working Version 2/1809MeetMe-3DuinoSerial/Update.cpp
@@ -0,0 +1,211 @@
+//
+//  Update.cpp
+//
+//  Created by Carl Turner on 20/9/18.
+//
+#include "Engine.h"
+
+void Engine::m_Update(unsigned long dt, unsigned long t)
+{
+/*
+   * LEVELLING UP MODE
+   */
+  if(m_mode == Modes::LEVELLING_UP)
+  {     
+    if ((t - m_ModeStartedAt) > m_TimeoutLevelUp)
+    {
+      m_mode = Modes::COUNTDOWN;
+    }
+  }//end Levelling_Up mode
+  
+  /*
+   * PLAYING MODE
+   */
+  if(m_mode == Modes::PLAYING)
+  {           
+   //Update the bullet positions and player button lights
+      for (int i = 0; i < m_NumPlayers; i++)
+      {
+        if (!m_Players[i].isExploding() && m_Players[i].getHasFired())
+        {
+          m_PlayerButtonLeds[i].SetMode(0); //turn off their button led
+          
+          if (m_Bullets[i].getIsBoosting()) //flash boost button while it's boosting.
+          {
+            m_PlayerBoostLeds[i].SetMode(2);
+          }
+          else if (int var = m_Bullets[i].getBoostsRemaining() <= 0)
+          {
+            m_PlayerBoostLeds[i].SetMode(0); //turn off boost led if they've boosted already
+          }
+          else 
+          {
+            m_PlayerBoostLeds[i].SetMode(1); //turn on boost button led
+          }
+          
+          m_Bullets[i].updateBullet(m_stripLengthArray[i], m_SlidePots[i].updateSlidePot(), t); 
+        }
+        else if (!m_Players[i].isExploding() && !m_Players[i].getHasFired()) //If player hasn't fired, we can't refer to the bullets because they haven't been associated with the player yet.
+        {
+            m_PlayerButtonLeds[i].SetMode(1); //set the button led on
+            m_PlayerBoostLeds[i].SetMode(0); //indicate that they can't push the boost button yet
+        }    
+        else if (m_Players[i].isExploding()) //turn off both their button leds to indicate they can't press either anymore.
+        {
+          m_PlayerButtonLeds[i].SetMode(0);
+          m_PlayerBoostLeds[i].SetMode(0); 
+        }
+      }
+
+   //update explosions
+   for (int i = 0; i < m_NumExplosions; i++)
+   {
+    if (m_Explosions[i].IsActive())
+    {
+      m_Explosions[i].Update(t);
+    }
+   }
+   
+    //Handle collisions etc
+    int explodeOnStrip; //which led strip is the explosion happening on?
+    float explodePosition;  //where on the led strip is the explosion happening?
+    //1. Loop through each bullet and check against each other bullet to see if they've collided. 
+    for (int i = 0; i < m_NumBullets; i++)
+    {
+      for (int j = 0; j < m_NumBullets; j++)
+      {
+        if (i == j)
+        {
+          //the bullet can't collide with itself. Do nothing
+        }
+        //don't need to check if j partB in flight, cos it'll get checked anyway since we're looping through each bullet for each bullet
+        else if (m_Bullets[i].BIsInFlight())  
+            {
+              //2. A collision occurs if bullet i partB has passed bullet j PartA.
+              if (!m_Players[j].isExploding() && (m_Bullets[i].getHeadBPos() <= m_Bullets[j].getHeadAPos()))
+              {
+                //3. Get the strip and position of the collision.
+                /*   We know that It's extremely unlikely, near impossible that both bullets will meet at the exact middle, 
+                 *   so we assume there'll only ever be one bullet with partB in flight. 
+                 *   And the collision will be on one of the strips that has a bullet PartB in flight.
+                 *   ie, not that player's strip.
+                 *   (Even with 100 strips, one bullet will almost always be first to cross the peak).
+                 */
+                  //whose bullet is this?
+
+                  m_Bullets[j].StopA();  //make sure bullet j partA is no longer rendered
+                  m_Players[j].setIsExploding();
+                  m_Players[j].setHasFired(); //to stop them firing after they're dead, if they haven't fired yet
+                  //don't turn off bullet i because we still want it to render on every other strip. 
+                  //We'll make sure it doesn't render on the strip with the explosion in render()
+                  explodePosition = m_Bullets[j].getHeadAPos();
+                  m_Explosions[m_CurrentExplosion].SetActive(t, j, explodePosition);                  
+                  m_CurrentExplosion++;
+                  m_LastExplosionStartedAt = t;
+                  //Update Score
+                  int scoreValue = (int)(explodePosition + 0.0); //typecast float to int
+                  m_ScoreController.AddScore(scoreValue, m_stripLengthArray[j]);
+
+              } //end if             
+            }//end else if b in flight
+           
+      }//end for
+    }//end for and END COLLISIONS 
+
+    if (m_ScoreController.GetTargetReached())
+    {
+      if(m_BestRounds == 0)
+      {
+        m_BestRounds = m_Level;
+      }
+      else if(m_Level < m_BestRounds)
+      {
+        m_BestRounds = m_Level;
+        m_NewBestRounds = true; //this is to tell us to throw a party when a new record is set. See Game_Over mode loop below.
+      }
+       m_mode = Modes::GAME_OVER;
+    }
+    else if (m_mode == Modes::PLAYING && ((m_CurrentExplosion >= m_NumExplosions - 1) && (t - m_LastExplosionStartedAt > 300))) //wait before changing modes, to help let people make transition
+    {
+        //turn off button leds
+        for (int i = 0; i < m_NumPlayers; i++)
+        {
+          m_PlayerButtonLeds[i].SetMode(0);
+           m_PlayerBoostLeds[i].SetMode(0); 
+        }
+       loadLevel();
+       m_ModeStartedAt = millis();
+       m_mode = Modes::LEVELLING_UP;
+    }
+  } //end PLAYING mode
+ 
+ 
+ /*
+   * COUNTDOWN MODE
+   */
+  if((m_mode == Modes::COUNTDOWN))
+  { 
+    if(!m_countDownStarted)
+    {
+      m_NewGameButtonLED.SetMode(0);
+      for (int i = 0; i < m_NumPlayers; i++)
+      {
+        m_PlayerBoostLeds[i].SetMode(0); 
+      }
+      
+      m_countDownStartedAt = t;
+      m_countDownStarted = true;
+    }
+    if (m_countDownStarted)
+    {       
+        if (t - m_countDownStartedAt > 3000)
+        {
+          m_mode = Modes::PLAYING;
+          m_countDownStarted = false;
+
+        //3 seconds countdown. See notes in Engine.h before declaration of m_CountDownDuration variable.
+        //+1 added to shift the countdown pixels up one pixel along the strip, 
+        //because the first pixel is actually hidden in the physical structure.
+        //so we stop the countdown once it gets to 1, not 0.
+          m_CountDownRemaining = m_CountDownDuration;
+        }
+        else if (t - m_countDownStartedAt > 2000)
+        {
+          m_CountDownRemaining = 1;
+        }
+        else if (t - m_countDownStartedAt > 1000)
+        {
+          m_CountDownRemaining = 2;
+        }
+        else if (t - m_countDownStartedAt > 0)
+        {
+          m_CountDownRemaining = 3;
+        }
+    }
+  } //end COUNTDOWN mode
+
+ /*
+   * GAME_OVER MODE
+   */
+  if((m_mode == Modes::GAME_OVER))
+  { 
+    //turn off boost leds
+    for (int i = 0; i < m_NumPlayers; i++)
+    {
+       m_PlayerBoostLeds[i].SetMode(0); 
+    }
+
+    //set New Game button LED to flashing
+    m_NewGameButtonLED.SetMode(2);
+    
+    //do some cool animation thing, celebration
+      
+    if(m_NewBestRounds)
+    {
+      //if they set a new 'best rounds', let them know.
+
+      m_NewBestRounds = false;
+    }
+  }
+
+} //end update()
